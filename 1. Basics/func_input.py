8f3993bea91e42d0e123ba856052069901e01351
Ved Bulsara <vedbulsara7@gmail.com>
2023-05-26 09:38:18 +0530

    commit 1.1 (26/5/26)

---
+++ 1. Basics/func_input.py
@@ -0,0 +1,7 @@
+# Input function in Python (Example-1)
+name = input("Enter your name : ")
+print("Your name is " + name)
+
+# Input function in Python (Example-1)
+print("Hello " + input("What is your name? ") + "!")
+
