3a6765733ceb9dd009251fe1fbe6704b79eff9ac
Ved <vedbulsara7@gmail.com>
2023-05-28 22:00:21 +0530

    commit 2.1 (28/5/23)

---
+++ 2. Conditional Statements/conceptual/if_else.py
@@ -0,0 +1,8 @@
+print("Welcome to Roller-coaster ride!")
+height = int(input("What is your height in cm ? : "))
+
+if height >= 120:
+    print("You can ride the roller-coaster!")
+else:
+    print("Sorry! You cannot ride the roller-coaster")
+
