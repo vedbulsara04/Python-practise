beba146df831a17a9524549a2e0a6ee9b0436db8
Ved <vedbulsara7@gmail.com>
2023-06-05 22:27:04 +0530

    commit 3.4 (5/6/23)

---
+++ 4. Datatypes/Lists/conceptual/nested_list.py
@@ -0,0 +1,7 @@
+# nested list
+
+team_name = ["Mercedes AMG Petronas Formula1 team"]
+driver_name = ["Lewis Hamilton", "George Russell"]
+
+favourite_team = [team_name, driver_name]
+print(favourite_team)
