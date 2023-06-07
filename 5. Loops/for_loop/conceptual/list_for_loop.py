f9bf8bcc9b65320b6543589d1eafe6c938bcf176
Ved <vedbulsara7@gmail.com>
2023-06-07 20:54:29 +0530

    commit 3.6 (7/6/23)

---
+++ 5. Loops/for_loop/conceptual/list_for_loop.py
@@ -0,0 +1,5 @@
+top_teams_f1_2023 = ["Mercedes AMG Petronas", "Oracle Red Bull Racing",
+                     "Scuderria Ferrari", "Aston Matin"]
+
+for team in top_teams_f1_2023:
+    print(team + " Formula1 team")
